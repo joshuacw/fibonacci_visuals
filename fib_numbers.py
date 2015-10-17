762a23d318e8b2af243cb80b064e56c81d14ef09
joshuacw <shua4387@yahoo.com>
2015-10-16 16:37:50 -1000

    implemented argparse

--- fib_numbers.py
+++ fib_numbers.py
@@ -1,21 +1,24 @@
-import scipy.constants
-import sys
+import scipy.constants, argparse
 
 phi = scipy.constants.golden
-term = int(sys.argv[1])
 fib_list = [0, 1]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("integer", type = int, help = "Type in the number of Fibonacci terms to be shown.")
+args = parser.parse_args()
+
+term = args.integer
 
 #implementing Binet's formula to produce the Nth term
 def nth_term(term):
     return int(round((phi**term - (1 - phi)**term) / 5**0.5))
 
-print "The number %d term in the Fibonacci sequence is %d." % (int(sys.argv[1]), nth_term(term))
+print "The number %d term in the Fibonacci sequence is %d." % (int(term), nth_term(term))
 
 def fib_terms(term): #a list producing the first N terms
-    for i in range(2, term + 1):
+    for i in range(2, term + 1): 
         fib_list.append(fib_list[i - 1] + fib_list[i - 2])
-    return fib_list
-
+    return fib_list 
 print ""
-print "The first terms of the Fibonacci sequence out to term %d are:" % int(sys.argv[1])
+print "The first terms of the Fibonacci sequence out to term %d are:" % int(term)
 print fib_terms(term)

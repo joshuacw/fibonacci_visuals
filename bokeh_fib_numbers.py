9085356887a7b5b93db434e3709a230a5cf84ccf
joshuacw <shua4387@yahoo.com>
2015-10-23 16:33:25 -1000

    added prime numbers to graph

--- bokeh_fib_numbers.py
+++ bokeh_fib_numbers.py
@@ -13,31 +13,51 @@ def nth_term(term):
 
 print "The number {0} term in the Fibonacci sequence is {1}.".format(int(args.integer), nth_term(args.integer))
 
-def fib_terms(term): #a list producing the first N terms
+def fib_list(term): #produces a list of the first N Fibonacci terms 
     fib_list = [0, 1]
     for i in range(2, term + 1): 
         fib_list.append(fib_list[i - 1] + fib_list[i - 2])
     return fib_list 
 
+def prime_list(term): #produces a list of the first N prime numbers
+    prime_list = [2]
+    number = 3
+
+    while len(prime_list) < term + 1:
+        counter = 0
+        for n in range(2, number):
+            if number % n == 0:
+                counter += 1
+            else:
+                counter += 0
+        if counter == 0:
+            prime_list.append(number)
+        number += 1
+    return prime_list
+
 #data for graph
 x = range(args.integer + 1)
-y = fib_terms(args.integer)
+y = fib_list(args.integer)
+y1 = prime_list(args.integer)
 
 #output to static HTML file
 output_file("lines.html", title="Growth of Fibonacci Sequence")
 
 # creating a new plot with title and axis labels
-p = figure(title="Growth of Fibonacci Sequence", x_axis_label='Term number', y_axis_label='Value')
+p = figure(title="Growth of Fibonacci Sequence vs. Prime numbers", x_axis_label='Term number', y_axis_label='Value')
 
 #add a line renderer with legend and line thickness
-p.line(x, y, legend="Fibonacci terms", line_width=2)
+p.line(x, y, legend="Fibonacci terms", line_width=2, line_color="red")
+p.line(x, y1, legend="Prime number terms", line_width=3, line_color="blue", line_dash="4 4")
 
 #show results
 show(p)
 
 print ""
 print "The first terms of the Fibonacci sequence out to term {} are:".format(int(args.integer))
-print fib_terms(args.integer)
+print fib_list(args.integer)
+print "The first terms of prime numbers out to term {} are:".format(int(args.integer))
+print prime_list(args.integer)
 print ""
 print "The value of the quotient of the last two terms in the sequence is: {}".format(float(y[args.integer]) / float(y[args.integer - 1]))
 print "The true value of phi is: {}".format(phi)

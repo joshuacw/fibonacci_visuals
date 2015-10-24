41ef58c5a89d3f05a7172905ed1d721d21133a63
joshuacw <shua4387@yahoo.com>
2015-10-23 14:01:31 -1000

    Fixed. Thanks Brian.

--- bokeh_fib_numbers.py
+++ bokeh_fib_numbers.py
@@ -2,7 +2,6 @@ import scipy.constants, argparse
 from bokeh.plotting import figure, output_file, show
 
 phi = scipy.constants.golden
-fib_list = [0, 1]
 
 parser = argparse.ArgumentParser(description="A visual display of the Fibonacci sequence.")
 parser.add_argument("integer", type = int, help = "Type in the number of Fibonacci terms to be shown.")
@@ -15,6 +14,7 @@ def nth_term(term):
 print "The number {0} term in the Fibonacci sequence is {1}.".format(int(args.integer), nth_term(args.integer))
 
 def fib_terms(term): #a list producing the first N terms
+    fib_list = [0, 1]
     for i in range(2, term + 1): 
         fib_list.append(fib_list[i - 1] + fib_list[i - 2])
     return fib_list 
@@ -27,7 +27,7 @@ y = fib_terms(args.integer)
 output_file("lines.html", title="Growth of Fibonacci Sequence")
 
 # creating a new plot with title and axis labels
-p = figure(title="Growth of Fibonacci Sequence", x_axis_label='x', y_axis_label='y')
+p = figure(title="Growth of Fibonacci Sequence", x_axis_label='Term number', y_axis_label='Value')
 
 #add a line renderer with legend and line thickness
 p.line(x, y, legend="Fibonacci terms", line_width=2)
@@ -39,5 +39,5 @@ print ""
 print "The first terms of the Fibonacci sequence out to term {} are:".format(int(args.integer))
 print fib_terms(args.integer)
 print ""
-print "The value of the quotient of the last two values in the sequence is: {}".format(float(fib_list[args.integer]) / float(fib_list[args.integer - 1]))
+print "The value of the quotient of the last two terms in the sequence is: {}".format(float(y[args.integer]) / float(y[args.integer - 1]))
 print "The true value of phi is: {}".format(phi)

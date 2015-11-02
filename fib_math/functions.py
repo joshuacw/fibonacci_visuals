dc05218b1189fd295c11a5eb1ab27db699ee5db8
joshuacw <shua4387@yahoo.com>
2015-11-02 05:58:56 -1000

    works for all int > 1

--- fib_math/functions.py
+++ fib_math/functions.py
@@ -1,10 +1,14 @@
 import scipy.constants
 import decimal
+decimal.getcontext().prec = 1000
+my_context = decimal.Context(prec=1000)
+decimal.setcontext(my_context)
 
 #implementing Binet's formula to produce the Nth term
 def nth_term(term):
     phi = scipy.constants.golden
-    return int(round((phi**term - (1 - phi)**term) / 5**0.5))
+    ten_places = decimal.Decimal(10) ** -10
+    return decimal.Decimal((phi**term - (1 - phi)**term) / 5**0.5).quantize(ten_places)
 
 #produces a list of the first N Fibonacci terms
 def fib_list(term): 
@@ -34,20 +38,24 @@ def prime_list(term):
 def phi_match(term):
     phi1000list = []
     phi_dec_list = []
+    ten_places = decimal.Decimal(10) ** -999
+#    my_context = decimal.Context(prec = 1001)
+    decimal.setcontext(my_context)
     with open('phi1000.txt', 'r') as phi:
         phi1000 = phi.read()
-        phi_dec = str(decimal.Decimal(nth_term(term)) / decimal.Decimal(nth_term(term - 1)))
 
         for char in phi1000:
             phi1000list.append(char)
 
+        phi_dec = str(decimal.Decimal(nth_term(term) / nth_term(term - 1)).quantize(ten_places))
         for char in phi_dec: 
             phi_dec_list.append(char)
 
-    for i in range(1001):
-        if(phi1000list[i] == phi_dec_list[i]):
-            i += 1
-        else:
-            break
+    i = 0
+    while(phi1000list[i] == phi_dec_list[i]):
+        i += 1
 
-    return i - 1
+    if term == 3:
+        return 0
+    else:
+        return i - 1

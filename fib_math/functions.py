916ae78dea8395789a7ca2bdcb31e6258aed2f93
joshuacw <shua4387@yahoo.com>
2015-11-10 00:05:24 -1000

    accuracy up to ~190 fib terms

--- fib_math/functions.py
+++ fib_math/functions.py
@@ -1,11 +1,10 @@
-import scipy.constants
 import decimal
 
-#implementing Binet's formula to produce the Nth term
 def nth_term(term):
-    phi = scipy.constants.golden
-    ten_places = decimal.Decimal(10) ** -10
-    return decimal.Decimal((phi**term - (1 - phi)**term) / 5**0.5).quantize(ten_places)
+    a, b = decimal.Decimal(0), decimal.Decimal(1)
+    for i in range(term):
+        a, b = b, a + b
+    return a
 
 #produces a list of the first N Fibonacci terms
 def fib_list(term): 
@@ -31,20 +30,17 @@ def prime_list(term):
         number += 1
     return prime_list
 
-# check how many digits quotient of last 2 terms matches phi out to
-def phi_match(term):
+def phi_match(term): 
     phi1000list = []
     phi_dec_list = []
-    thousand_places = decimal.Decimal(10) ** -999
-    my_context = decimal.Context(prec=1000)
-    decimal.setcontext(my_context)
+    decimal.getcontext().prec = 1000
     with open('phi1000.txt', 'r') as phi:
         phi1000 = phi.read()
 
         for char in phi1000:
             phi1000list.append(char)
 
-        phi_dec = str(decimal.Decimal(nth_term(term) / nth_term(term - 1)).quantize(thousand_places))
+        phi_dec = str(decimal.Decimal(nth_term(term) / nth_term(term - 1))) 
         for char in phi_dec: 
             phi_dec_list.append(char)
 

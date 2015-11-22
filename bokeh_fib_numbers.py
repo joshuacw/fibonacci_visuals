b461a6c965540f8bdf5318633c901cb153312459
joshuacw <shua4387@yahoo.com>
2015-11-22 04:06:53 -1000

    added a visual

--- bokeh_fib_numbers.py
+++ bokeh_fib_numbers.py
@@ -1,7 +1,7 @@
 import scipy.constants
 import argparse
+from bokeh.io import hplot
 from bokeh.plotting import figure, output_file, show
-from bokeh.charts import Histogram, output_file, show
 from fib_math.functions import nth_term, fib_list, prime_list, phi_compare
 
 phi = scipy.constants.golden
@@ -13,34 +13,39 @@ parser.add_argument("integer", type = int, help = \
 parser.add_argument('--debug', '--d', action = 'store_true')
 args = parser.parse_args()
 
-#data for graphs
-#x = range(args.integer + 1)
-#y = fib_list(args.integer)
-#y1 = prime_list(args.integer)
+#data for growth plot 
+x_growth = range(args.integer + 1)
+y_fib = fib_list(args.integer)
+y_prime = prime_list(args.integer)
 
-x1 = range(2, args.integer + 1)
-y2 = map(phi_compare, range(2, args.integer + 1)) 
+#data for phi-comparison plot
+x_phi = range(2, args.integer + 1)
+y_phi = map(phi_compare, range(2, args.integer + 1)) 
 
 #output to static HTML file
-#output_file("lines.html", title="Growth of Fibonacci Sequence")
-output_file("histogram.html")
+output_file("layout.html", title="Fibonacci Fun")
 
-# creating Fibonacci growth plot and phi plot with title and axis labels
-#p = figure(title="Growth of Fibonacci Sequence vs. Prime numbers", \
-        #x_axis_label='Term number', y_axis_label='Value')
-p = Histogram(x1, y2, title="Convergence of Approx. to True Value of Phi") 
+# creating Fibonacci and prime number growth plot 
+s1 = figure(plot_width=600, plot_height=600, title="Growth of Fibonacci Sequence vs. Prime numbers", \
+        x_axis_label='Term number', y_axis_label='Value')
+s1.line(x_growth, y_fib, legend="Fibonacci terms", line_width=2, line_color="red")
+s1.line(x_growth, y_prime, legend="Prime number terms", \
+        line_width=3, line_color="blue", line_dash="4 4")
 
-#add a line renderer with legend and line thickness
-#p.line(x, y, legend="Fibonacci terms", line_width=2, line_color="red")
-#p.line(x, y1, legend="Prime number terms", \
-        #line_width=3, line_color="blue", line_dash="4 4")
+#Create phi-comparison plot
+s2 = figure(plot_width=600, plot_height=600, title='Phi Estimate vs. Phi', \
+        x_axis_label= 'Fibonacci term number', y_axis_label='Matching Digit')
+s2.circle(x_phi, y_phi, size=15, color='black', alpha=0.5) 
+
+#Put plots in an HBox
+p = hplot(s1, s2)
 
 #show results
 show(p)
 
 print ""
 print "The number {0} term in the Fibonacci sequence is {1}."\
-        .format(int(args.integer), y[args.integer])
+        .format(int(args.integer), y_fib[args.integer])
 print ""
 print "The first terms of the Fibonacci sequence out to term {} are:"\
         .format(int(args.integer))
@@ -51,7 +56,7 @@ print "The first terms of prime numbers out to term {} are:"\
 print prime_list(args.integer)
 print ""
 print "The value of the quotient of the last two terms in the sequence is: {}"\
-        .format(float(y[args.integer]) / float(y[args.integer - 1]))
+        .format(float(y_fib[args.integer]) / float(y_fib[args.integer - 1]))
 print "The true value of phi is: {}".format(phi)
 print ""
 print "The quotient of the last two terms of the sequence match the true value of phi out to {} digits."\
